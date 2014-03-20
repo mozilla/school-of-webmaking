dcdd0df5b19ad21452c6b037bcde243f07499ebb
Dirk Uys <dirkcuys@gmail.com>
2014-03-20 14:18:14 -0300

    Update discourse integration to only show participants if there are more than one. Remove github edit button.

--- js/discourse.js
+++ js/discourse.js
@@ -3,27 +3,32 @@ function write_discourse_div(data, topic_url)
 {
     $('#discourse').html('<div id="comments"></div><div id="participants"></div>');
     var part_div = $('#discourse #participants');
-    part_div.append( '<p>' + data.participants.length + ' people are talking about this. Join the <a href="' + topic_url + '">conversation</a>.</p>');
-    part_div.append('<p>');
-    $.each(data.participants, function (i, participant) {
-        var avatar_url = participant.avatar_template.replace('\{size\}', "45");
-        part_div.append('<img src="' + avatar_url + '">');
-    });
-    part_div.append('</p>');
+    if (data.participants.lenght > 1){
+        part_div.append( '<p>' + data.participants.length + ' people are talking about this.</p>');
+        part_div.append('<p>');
+        $.each(data.participants, function (i, participant) {
+            var avatar_url = participant.avatar_template.replace('\{size\}', "45");
+            part_div.append('<img src="' + avatar_url + '">');
+        });
+        part_div.append('</p>');
+        part_div.append( '<p><a class="btn btn-default" href="' + topic_url + '">Join the conversation</a></p>');
 
-    var comment_div = $('#discourse #comments');
-    $.each(data.posts, function(i, post){
-        comment = '<div class="media">';
-        comment += '<a class="pull-left">';
-        avatar_url = post.avatar_template.replace('\{size\}', "45");
-        comment += '<img class="media-object" src="' + avatar_url;
-        comment += '" alt="Profile image of ' + post.name + '">'
-        comment += '</a>';
-        comment += '<div class="media-body">';
-        comment += post.cooked;
-        comment += '</div></div>';
-        comment_div.append(comment);
-    });
+        var comment_div = $('#discourse #comments');
+        $.each(data.posts, function(i, post){
+            comment = '<div class="media">';
+            comment += '<a class="pull-left">';
+            avatar_url = post.avatar_template.replace('\{size\}', "45");
+            comment += '<img class="media-object" src="' + avatar_url;
+            comment += '" alt="Profile image of ' + post.name + '">'
+            comment += '</a>';
+            comment += '<div class="media-body">';
+            comment += post.cooked;
+            comment += '</div></div>';
+            comment_div.append(comment);
+        });
+    } else {
+        part_div.append( '<p><a class="btn btn-default" href="' + topic_url + '">Start the conversation</a></p>');
+    }
 }
 
 function write_discourse_section(topic_url){
